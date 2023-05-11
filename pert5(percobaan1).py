44af9db7fdaf6e52858086bfecd57d5d59f9d01b
heruramdani <hermdn16@gmail.com>
2023-05-11 13:54:21 +0700

    Tugas pcd pert 5

--- pert5(percobaan1).py
+++ pert5(percobaan1).py
@@ -1,30 +1,32 @@
-
+#Mengimport library yang diperlukan yaitu cv2, matplotlib, skimage.data, skimage.io, skimage.color, dan skimage.util.
 import cv2
 import matplotlib.pyplot as plt
 from skimage import data
 from skimage.io import imread
 from skimage.color import rgb2gray
 from skimage.util import invert
-
+#Mengimport numpy dan memberikan alias np pada numpy.
 import numpy as np
 
 #Percobaan 1
-astronautImage = imread('aadc.jpeg')
+# Load and crop two images
+astronautImage = imread('aadc.jpeg')#Membaca citra dengan menggunakan imread() pada library skimage.io dan dimasukkan ke dalam variabel astronautImage dan cameraImage.
 cameraImage = imread('uin.png')
 
-astroCropped = astronautImage.copy()
+astroCropped = astronautImage.copy()#Melakukan cropping pada citra astronautImage dan cameraImage dengan copy() dan slicing pada indeks piksel untuk mendapatkan bagian dari citra yang diinginkan.
 astroCropped = astroCropped[0:256,64:320]
 
-cameraCropped = cameraImage.copy()
+cameraCropped = cameraImage.copy()#Melakukan cropping pada citra astronautImage dan cameraImage dengan copy() dan slicing pada indeks piksel untuk mendapatkan bagian dari citra yang diinginkan.
 cameraCropped = cameraCropped[64:256,128:320]
 
-print('Astro Ori Shape : ',astronautImage.shape)
+# Print the shapes of the original and cropped images
+print('Astro Ori Shape : ',astronautImage.shape)#Menampilkan bentuk matriks dari citra asli dan citra hasil cropping menggunakan shape.
 print('Astro Crop Shape : ',astroCropped.shape)
 
 print('Camera Ori Shape : ',cameraImage.shape)
 print('Camera Crop Shape : ',cameraCropped.shape)
-
-fig, axes = plt.subplots(2, 2, figsize=(12, 12))
+# Display the original and cropped images using subplots
+fig, axes = plt.subplots(2, 2, figsize=(12, 12))#Membuat plot gambar menggunakan subplot dengan ukuran 2x2 dengan judul yang sudah diberikan. Terdapat 4 gambar, 2 gambar asli dan 2 gambar hasil cropping yang telah disiapkan.
 ax = axes.ravel()
 
 ax[0].imshow(astronautImage)
@@ -41,10 +43,13 @@ ax[3].set_title('Citra Output 2')
 #plt.show()
 
 #percobaan 2
+#Invert the input image
 inv = invert(astroCropped)
+#Print the input and output image shape
 print('Shape Input : ', astroCropped.shape)
 print('Shape Output : ',inv.shape)
 
+#Plot the input and output images along with their histograms
 fig, axes = plt.subplots(2, 2, figsize=(12, 12))
 ax = axes.ravel()
 
@@ -62,17 +67,18 @@ ax[3].set_title('Histogram Output')
 #plt.show()
 
 #percobaan 3
+#Meng-copy citra cameraCropped ke dalam variabel copyCamera dengan tipe data float
 copyCamera = cameraCropped.copy().astype(float)
-
+#Mendapatkan ukuran citra dan membuat array kosong dengan ukuran yang sama dengan citra
 shape = copyCamera.shape
 output1 = np.empty(shape)
-
+#Looping untuk setiap piksel pada citra
 for baris in range(0, shape[0] - 1):
-    for kolom in range(0, shape[1] - 1):
+    for kolom in range(0, shape[1] - 1):# Memperoleh koordinat piksel pada citra yang akan diubah nilai
         a1 = baris
         b1 = kolom
-        output1[a1, b1] = copyCamera[baris, kolom] / 192
-
+        output1[a1, b1] = copyCamera[baris, kolom] / 192# Menghitung nilai brightness baru dengan membagi nilai brightness pada
+#Menampilkan citra input, histogram input, citra output brightness, dan histogram output
 fig, axes = plt.subplots(2, 2, figsize=(12, 12))
 ax = axes.ravel()
 
@@ -84,5 +90,5 @@ ax[2].imshow(output1, cmap='gray')
 ax[2].set_title('Citra Output (Brightnes)')
 ax[3].hist(output1.ravel(), bins=192)
 ax[3].set_title('Histogram Input')
-print(output1.shape)
+print(output1.shape)#Menampilkan ukuran citra output pada variabel output1 dan menampilkan gambar
 plt.show()
--- prak5(percobaan 2).py
+++ prak5(percobaan 2).py
@@ -1,140 +1,143 @@
-import numpy as np
+import numpy as np #Mengimport library NumPy dengan alias np untuk melakukan operasi array pada gambar.
 import imageio
-import cv2
-import matplotlib.pyplot as plt
+import cv2#Mengimport library OpenCV untuk melakukan operasi gambar.
+import matplotlib.pyplot as plt#Mengimport library Matplotlib untuk menampilkan gambar.
 
-img = cv2.imread("aadc.jpeg")
+img = cv2.imread("aadc.jpeg")#Membaca gambar dengan nama "aadc.jpeg" menggunakan fungsi imread()
 
-img_height =img.shape[0]
-img_width = img.shape[1]
-img_channel = img.shape[2]
-img_type = img.dtype
+img_height =img.shape[0]# Mengambil ukuran tinggi gambar dan memasukkannya ke dalam variabel img_height.
+img_width = img.shape[1]#Mengambil ukuran lebar gambar dan memasukkannya ke dalam variabel img_width.
+img_channel = img.shape[2]#Mengambil jumlah channel gambar dan memasukkannya ke dalam variabel img_channel.
 
-img_brightness = np.zeros(img.shape, dtype= np.uint8)
+img_type = img.dtype#Mengambil tipe data gambar dan memasukkannya ke dalam variabel img_type.
 
-def brighter(nilai):
-    for y in range(0, img_height):
-        for x in range(0, img_width):
-            red= img[y][x][0]
-            green= img[y][x][1]
-            blue= img[y][x][2]
-            gray= (int(red)+ int(green)+int(blue))/3
-            gray += nilai
-            if gray >255:
-                gray= 255
-            if gray<0:
-                gray = 0
-            img_brightness[y][x] = (gray, gray, gray)
+img_brightness = np.zeros(img.shape, dtype= np.uint8)#Membuat array kosong dengan ukuran yang sama dengan gambar dan menetapkan tipe data uint8
+
+def brighter(nilai):#Mendefinisikan fungsi brighter dengan satu parameter input nilai.
+    for y in range(0, img_height):#Looping untuk setiap baris (y) pada gambar.
+        for x in range(0, img_width):# Looping untuk setiap kolom (x) pada gambar.
+            red= img[y][x][0]#Mendapatkan nilai pixel merah pada koordinat (x, y).
+            green= img[y][x][1]# Mendapatkan nilai pixel hijau pada koordinat (x, y).
+            blue= img[y][x][2]#Mendapatkan nilai pixel biru pada koordinat (x, y).
+            gray= (int(red)+ int(green)+int(blue))/3#Menghitung nilai rata-rata dari ketiga channel warna pada pixel (x, y).
+            gray += nilai#Menambahkan nilai parameter input nilai ke dalam nilai rata-rata warna pada pixel (x, y).
+            if gray >255:#Mengecek apakah nilai gray lebih besar dari 255.
+                gray= 255# Jika nilai gray lebih besar dari 255, maka nilai gray akan diset menjadi 255.
+            if gray<0:#Mengecek apakah nilai gray kurang dari 0.
+                gray = 0#Jika nilai gray kurang dari 0, maka nilai gray akan diset menjadi 0.
+            img_brightness[y][x] = (gray, gray, gray)#Menetapkan nilai pixel baru pada koordinat (x, y) pada gambar hasil.
 #menampilkan gambar dengan parameter -100
-brighter(-100)
-plt.imshow(img_brightness)
+brighter(-100)# Memanggil fungsi brighter dengan parameter input -100.
+plt.imshow(img_brightness)#Menampilkan gambar hasil pada plot.
 plt.title("brightness -100")
 plt.show()
 
-brighter(25)
-plt.imshow(img_brightness)
+brighter(25)# Memanggil fungsi brighter dengan parameter input 25.
+plt.imshow(img_brightness)#Menampilkan gambar hasil pada plot.
 plt.title("Brigtness 100")
 plt.show()
 
 #rgb
-img_rgbbright = np.zeros(img.shape, dtype=np.uint8)
+img_rgbbright = np.zeros(img.shape, dtype=np.uint8)#membuat array numpy kosong dengan dimensi yang sama dengan citra asli dan tipe data unsigned integer 8-bit.
 #fungsi untuk brightness rgb dengan nilai parameter
 
-def rgbbrighter(nilai):
+def rgbbrighter(nilai):#mendefinisikan fungsi rgbbrighter yang menerima parameter nilai.
     for y in range(0, img_height):
-        for x in range(0, img_width):
-            red=img[y][x][0]
-            red+= nilai
+        for x in range(0, img_width):#erasi semua pixel pada gambar dengan mengakses nilai baris y dan kolom x dari gambar.
+            red=img[y][x][0]#mengambil nilai kanal merah untuk pixel di koordinat (x, y).
+            red+= nilai#menambahkan nilai parameter nilai ke kanal merah untuk pixel di koordinat (x, y).
             if red > 255:
                 red = 255
             if red < 0:
-                red = 0
-            green = img[y][x][1]
-            green += nilai
+                red = 0#memastikan bahwa nilai kanal merah tidak melebihi 255 atau kurang dari 0.
+            green = img[y][x][1]#mengambil nilai kanal hijau dan biru untuk pixel di koordinat (x, y).
+            green += nilai#menambahkan nilai parameter nilai ke kanal hijau dan biru untuk pixel di koordinat (x, y).
             if green > 255:
                 green = 255
             if green < 0:
-                green = 0
-            blue = img[y][x][2]
-            blue += nilai
+                green = 0# memastikan bahwa nilai kanal hijau dan biru tidak melebihi 255 atau kurang dari 0.
+            blue = img[y][x][2]#mengambil nilai kanal hijau dan biru untuk pixel di koordinat (x, y).
+            blue += nilai#menambahkan nilai parameter nilai ke kanal hijau dan biru untuk pixel di koordinat (x, y).
             if blue > 255:
                 blue = 255
             if blue < 0:
-                blue = 0
-            img_rgbbright[y][x] = (red, green, blue)
+                blue = 0# memastikan bahwa nilai kanal hijau dan biru tidak melebihi 255 atau kurang dari 0.
+            img_rgbbright[y][x] = (red, green, blue)#menetapkan nilai piksel yang diubah ke array gambar yang baru dibuat.
 #menampilkan
-rgbbrighter(-100)
-plt.imshow(img_rgbbright)
-plt.title("Brightness -100")
-plt.show()
-rgbbrighter(100)
-plt.imshow(img_rgbbright)
-plt.title("Brightness 100")
-plt.show()
+rgbbrighter(-100)#memanggil fungsi rgbbrighter dengan parameter -100 dan 100
+plt.imshow(img_rgbbright)#enampilkan citra baru yang dihasilkan.
+plt.title("Brightness -100")# menampilkan judul citra yang sesuai untuk masing-masing citra baru.
+plt.show()#menampilkan citra pada plot matplotlib.
+rgbbrighter(100)#memanggil fungsi rgbbrighter dengan parameter -100 dan 100
+plt.imshow(img_rgbbright)#enampilkan citra baru yang dihasilkan.
+plt.title("Brightness 100")#menampilkan judul citra yang sesuai untuk masing-masing citra baru.
+plt.show()#menampilkan citra pada plot matplotlib.
 
+#membuat matriks nol dengan ukuran yang sama dengan gambar
 img_contrass = np.zeros(img.shape, dtype=np.uint8)
+#fungsi untuk kontras dengan nilai parameter
 def contrass(nilai):
     for y in range(0, img_height):
-        for x in range(0, img_width):
+        for x in range(0, img_width):# mendapatkan nilai RGB dari setiap piksel gambar
             red = img[y][x][0]
             green = img[y][x][1]
-            blue = img[y][x][2]
-            gray = (int(red) + int(green) + int(blue)) / 3
-            gray *= nilai
+            blue = img[y][x][2]## menghitung nilai rata-rata RGB untuk membuat citra abu-abu
+            gray = (int(red) + int(green) + int(blue)) / 3# mengalikan citra abu-abu dengan nilai kontras
+            gray *= nilai# menentukan nilai maksimum 255 untuk masing-masing komponen warna
             if gray > 255:
-                gray = 255
+                gray = 255# menyimpan nilai kontras baru untuk setiap piksel gambar
             img_contrass[y][x] = (gray, gray, gray)
 
-# menampilkan
+# menampilkan gambar dengan kontras 2
 contrass(2)
 plt.imshow(img_contrass)
 plt.title("Contrass 2")
 plt.show()
-
+#menampilkan gambar dengan kontras 3
 contrass(3)
 plt.imshow(img_contrass)
 plt.title("Contrass 3")
 plt.show()
 
-img_contrasrgb = np.zeros(img.shape, dtype=np.uint8)
+img_contrasrgb = np.zeros(img.shape, dtype=np.uint8)#Membuat array kosong dengan ukuran yang sama dengan gambar input
 # fungsi rgb
-def rgbcontrass(nilai):
-    for y in range(0, img_height):
+def rgbcontrass(nilai):#Fungsi untuk menambahkan kontras pada gambar dengan parameter nilai
+    for y in range(0, img_height):## Looping untuk setiap pixel pada gambar
         for x in range(0, img_width):
             red = img[y][x][0]
             red += nilai
             if red > 255:
-                red = 255
-            green = img[y][x][1]
+                red = 255#memastikan bahwa nilai kanal merah tidak melebihi 255 .
+            green = img[y][x][1]#memastikan bahwa nilai kanal merah tidak melebihi 255 .
             green += nilai
             if green > 255:
-                green = 255
-            blue = img[y][x][2]
+                green = 255#memastikan bahwa nilai kanal hijau tidak melebihi 255 .
+            blue = img[y][x][2]#memastikan bahwa nilai kanal merah tidak melebihi 255 .
             blue += nilai
             if blue > 255:
-                blue = 255
+                blue = 255#memastikan bahwa nilai kanal biru tidak melebihi 255 .
             img_contrasrgb[y][x] = (red, green, blue)
 
 
 # menampilkan
-rgbcontrass(20)
+rgbcontrass(20)#Menambahkan kontras pada gambar dengan nilai 20
 plt.imshow(img_contrasrgb)
 plt.title("contrass 20")
 plt.show()
-rgbcontrass(100)
+rgbcontrass(100)#Menambahkan kontras pada gambar dengan nilai 100
 plt.imshow(img_contrasrgb)
 plt.title("contrass 100")
 plt.show()
 
 
 #autocontrass
-img_autocontrass = np.zeros(img.shape, dtype=np.uint8)
+img_autocontrass = np.zeros(img.shape, dtype=np.uint8)#Membuat array kosong dengan tipe data uint8 berukuran sama dengan citra
 
-def autocontrass():
+def autocontrass():#Fungsi untuk kontras otomatis
     xmax = 300
     xmin = 0
     d = 0
-
+    # Mencari nilai maksimum dan minimum dari citra
     for y in range(0, img_height):
         for x in range(0, img_width):
             red = img[y][x][0]
@@ -145,7 +148,9 @@ def autocontrass():
                 xmax = gray
             if gray > xmin:
                 xmin = gray
+    # Menghitung faktor skala
     d = xmin-xmax
+    # Menetapkan nilai kontras baru pada setiap piksel
     for y in range(0, img_height):
         for x in range(0, img_width):
             red = img[y][x][0]
@@ -154,6 +159,7 @@ def autocontrass():
             gray = (int(red) + int(green) + int(blue)) / 3
             gray = int(float(255/d) * (gray-xmax))
             img_autocontrass[y][x] = (gray, gray, gray)
+#Memanggil fungsi autocontrass() untuk menghasilkan gambar kontras otomatis
 autocontrass()
 plt.imshow(img_autocontrass)
 plt.title("Contrass Autolevel")
